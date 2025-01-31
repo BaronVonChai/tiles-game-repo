afab876748e9d1e9fdc9efd0ceda0b4efad04e2c
Alex Ford <alex.ford@nationalarchives.gov.uk>
2025-01-31 15:20:10 +0000

    intial commit

---
+++ Scenes/main_menu.gd
@@ -0,0 +1,25 @@
+class_name MainMenu
+extends Control
+
+@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
+@onready var options_button = $MarginContainer/HBoxContainer/VBoxContainer/Options_Button as Button
+@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
+
+
+
+func _ready():
+	start_button.button_down.connect(on_start_button_down)
+	options_button.button_down.connect(on_options_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+	
+func on_start_button_down() -> void:
+	print("start button pressed")
+	pass
+	
+func on_options_pressed() -> void:
+	print("options button pressed")
+	pass
+	
+func on_exit_pressed() -> void:
+	get_tree().quit()
+	
