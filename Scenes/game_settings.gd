ea010b910cff973a3757492f100f0c80829057a1
Alex Ford <alex.ford@nationalarchives.gov.uk>
2025-02-20 10:58:00 +0000

    fix : add two intial players as minimum

--- Scenes/game_settings.gd
+++ Scenes/game_settings.gd
@@ -6,19 +6,14 @@ const PlayerOptions = preload("res://Scripts/PlayerOptions.gd")
 @onready var add_player_button = $TopContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/AddPlayerButton as Button
 
 func _ready() -> void:
+	_on_add_player_button()
+	_on_add_player_button()
 	add_player_button.button_down.connect(_on_add_player_button)
 
 func _process(delta: float) -> void:
 	pass
 
-# In your GameSettings script
+# In your GameSettings scripcd t
 func _on_add_player_button() -> void:
-	if bottom_hbox.get_child_count() == 0:
-		# Add initial two players if none exist
-		var player1 = PlayerOptions.new()
-		var player2 = PlayerOptions.new()
-		player1.create_player_container(1, bottom_hbox)
-		player2.create_player_container(2, bottom_hbox)
-	else:
 		var new_player = PlayerOptions.new()
 		new_player.create_player_container(bottom_hbox.get_child_count() + 1, bottom_hbox)
